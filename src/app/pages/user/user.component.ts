07965846a8bbd27c33db6072faf8b4ed95570e7e
john@home <johninquest@gmail.com>
2024-05-16 00:15:58 +0200

    added simple delay

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -3,21 +3,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { COUNTRIES } from '../../shared/countries.list';
 // import { IdbService } from '../../utils/idb.service';
 
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
-export class UserComponent  { 
+export class UserComponent {
   // constructor(private _idb: IdbService) {}
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.getUserData();
     this.userForm.disable();
     this.userForm.patchValue({
-      country: 'Germany'
-    })
+      country: 'Germany',
+    });
   }
 
   userId: any;
@@ -36,41 +35,40 @@ export class UserComponent  {
   countryList: string[] = COUNTRIES;
 
   onClickCancel() {
-    history.back()
+    history.back();
   }
 
   onClickSave() {
-    alert('Tapped save button!');
-    console.log('UserData', this.userForm.value); 
-    console.log('UserData Type', typeof this.userForm.value);  
+    // alert('Tapped save button!');
+    console.log('UserData', this.userForm.value);
+    console.log('UserData Type', typeof this.userForm.value);
     // let userData = this.userForm.value;
     let userData = JSON.stringify(this.userForm.value);
     localStorage.setItem('user_data', userData);
     /* this._idb.addData(userData).then(res => console.log('addResponse:', res)) */
-  } 
+    setTimeout(() => this.userForm.disable(), 1000);
+  }
 
   getUserData() {
-    let storedData = localStorage.getItem('user_data'); 
-    if(storedData) {
-      console.log('Stored data:', storedData)
+    let storedData = localStorage.getItem('user_data');
+    if (storedData) {
+      console.log('Stored data:', storedData);
       let parsedData = JSON.parse(storedData);
       this.userForm.patchValue({
-        firstName: parsedData['firstName'],  
-        lastName: parsedData['lastName'],  
-        dateOfBirth: parsedData['dateOfBirth'], 
-        street: parsedData['street'], 
-        postCode: parsedData['postCode'], 
-        city: parsedData['city'], 
-        country: parsedData['country'], 
+        firstName: parsedData['firstName'],
+        lastName: parsedData['lastName'],
+        dateOfBirth: parsedData['dateOfBirth'],
+        street: parsedData['street'],
+        postCode: parsedData['postCode'],
+        city: parsedData['city'],
+        country: parsedData['country'],
       });
-    }else { 
-      console.log('No user data stored!')
-
+    } else {
+      console.log('No user data stored!');
     }
   }
 
   onClickEdit() {
-    this.userForm.enable()
+    this.userForm.enable();
   }
-
 }
