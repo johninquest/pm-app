45715b8898bc2e8cdcbedda9498b09afc7ca49c5
Jinq <43062557+johninquest@users.noreply.github.com>
2024-10-04 02:42:40 +0200

    translated error messages

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -11,14 +11,14 @@ import { AlertDialogComponent } from '../dialogs/alert-dialog/alert-dialog.compo
   templateUrl: './email.component.html',
   styleUrl: './email.component.scss'
 })
-export class EmailComponent { 
+export class EmailComponent {
   constructor(
     private _router: Router,
     private _fbAuthService: AuthService,
     private _dialog: MatDialog
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   hidePassword: boolean = true;
 
@@ -45,19 +45,22 @@ export class EmailComponent {
         })
         .catch((e: any) => {
           if (e.code === 'auth/invalid-email') {
-            let _message: string = 'Ungültige E-Mail-Adresse!';
+            // let _message: string = 'Ungültige E-Mail-Adresse!';
+            let _message: string = 'Invalid email address!';
             return this.openDialog(_message);
           }
           if (e.code === 'auth/user-not-found') {
-            let _message: string = 'Benutzer nicht gefunden!';
+            // let _message: string = 'Benutzer nicht gefunden!'; 
+            let _message: string = 'User not found!';
             return this.openDialog(_message);
           }
           if (e.code === 'auth/wrong-password') {
-            let _message: string = 'Falsches Passwort!';
+            // let _message: string = 'Falsches Passwort!'; 
+            let _message: string = 'Incorrect password!';
             return this.openDialog(_message);
           } else {
-            let _message: string = `Ein Fehler ist aufgetreten: \n${e.code} \n${e.message}`;
-
+            // let _message: string = `Ein Fehler ist aufgetreten: \n${e.code} \n${e.message}`; 
+            let _message: string = `An error has occurred: \n${e.code} \n${e.message}`;
             return this.openDialog(_message);
           }
         });
