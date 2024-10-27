365a86a72211e3c0619faf8a260e32430ea4a3d6
Jinq <43062557+johninquest@users.noreply.github.com>
2024-10-27 16:16:45 +0100

    improved expense page

--- src/app/pages/expenses/expense-create/expense-create.component.ts
+++ src/app/pages/expenses/expense-create/expense-create.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EXPENSE_TYPE_LIST, PROPERTY_LIST } from '../../../shared/lists/dummy.list';
+import { PbService } from '../../../utils/pb.service';
+import { AuthService } from '../../../utils/auth.service';
 
 @Component({
   selector: 'app-expense-create',
@@ -10,21 +12,36 @@ import { EXPENSE_TYPE_LIST, PROPERTY_LIST } from '../../../shared/lists/dummy.li
 export class ExpenseCreateComponent {
 
   expenseForm: FormGroup;
+  propertiesData: any[] = [];
+  expenseList: string[] = EXPENSE_TYPE_LIST;
+  // propertyList: string[] = PROPERTY_LIST;
 
-  constructor(private fb: FormBuilder) {
+  // Getter for property names
+  get propertyList(): string[] {
+    return this.propertiesData.map(property => property.name);
+  }
+
+  constructor(private fb: FormBuilder, private _pbService: PbService, private _fbAuth: AuthService) {
     this.expenseForm = this.fb.group({
       propertyId: ['', Validators.required],
+      propertyName: ['', Validators.required],
       expenseDate: [''],
       expenseType: [''],
       description: [''],
       amount: [0, [Validators.required, Validators.min(0)]],
       vendorName: ['']
     });
+    this._fbAuth.currentlyLoggedUser().subscribe((res) => {
+      let currentUser: string = res?.email ?? '';
+      if (currentUser) {
+        let pData = this._pbService.getAllPropertyAsList(currentUser);
+        pData.then(data => {
+          console.log('Property data', data)
+          this.propertiesData = data
+        });
+      }
+    });
   }
-
-  expenseList: string[] = EXPENSE_TYPE_LIST; 
-  propertyList: string[] = PROPERTY_LIST;
-
 
   onSubmit() {
     if (this.expenseForm.valid) {
