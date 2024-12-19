b9a1ddf48455d5a06fab1f9901cf7d6a6b1e52fb
Jinq <43062557+johninquest@users.noreply.github.com>
2024-12-19 12:01:06 +0100

    deactivated rent collect button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,31 +5,34 @@ import { PbAuthService } from '../../utils/pocketbase/pb-auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   constructor(private _router: Router, private _pbAuthService: PbAuthService) {}
 
-/*   onClickLogout() {
-    this._router.navigateByUrl('')
+  /*   onClickLogout() {
+      this._router.navigateByUrl('')
+  
+    } */
 
-  } */
-
-/*   onClickLogOut() {
-    let logoutResponse = this._fbAuthService.logUserOut();
-    logoutResponse
-      .then(() => {
-        sessionStorage.removeItem('popati_access_token');
-        // sessionStorage.removeItem('db_name');
-        this._router.navigateByUrl('/auth');
-      })
-      .catch((e) =>
-        console.log('The following error occured during logout =>', e)
-      );
-  } */ 
+  /*   onClickLogOut() {
+      let logoutResponse = this._fbAuthService.logUserOut();
+      logoutResponse
+        .then(() => {
+          sessionStorage.removeItem('popati_access_token');
+          // sessionStorage.removeItem('db_name');
+          this._router.navigateByUrl('/auth');
+        })
+        .catch((e) =>
+          console.log('The following error occured during logout =>', e)
+        );
+    } */
 
   onClickLogOut() {
     this._pbAuthService.logout();
   }
 
+  underConstruction() {
+    alert('Still under construction');
+  }
 }
