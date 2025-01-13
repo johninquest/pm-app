d262a7c0e010735e05ecfee5c16057b84fdc783d
john.dev <johninquest@gmail.com>
2025-01-13 02:46:51 +0100

    deleted all test files

--- src/app/shared/pipes/property-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PropertyTypePipe } from './property-type.pipe';
-
-describe('PropertyTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PropertyTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
